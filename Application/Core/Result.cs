f3c261103d5421230fd78c52e3358b6d952fa0e6
mastertope <tupengabs@gmail.com>
2023-09-05 23:39:08 +0800

    Section 10: API Fluentvalidation and Error Handling

---
+++ Application/Core/Result.cs
@@ -0,0 +1,13 @@
+namespace Application.Core
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Value { get; set; }
+        public string Error { get; set; }
+
+        public static Result<T> Success(T value) => new() { Value = value, IsSuccess = true};
+        public static Result<T> Failure(string error) => new() { Error = error, IsSuccess = false};
+
+    }
+}
